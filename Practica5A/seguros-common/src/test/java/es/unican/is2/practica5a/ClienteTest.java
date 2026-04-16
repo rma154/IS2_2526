b6046e5c7e09c7747fd8b30cbb15147a0236ad4c
rma154 <rma154@alumnos.unican.es>
2026-04-16 12:15:35 +0200

    Avances Practica5A: Inicio

---
+++ Practica5A/seguros-common/src/test/java/es/unican/is2/practica5a/ClienteTest.java
@@ -0,0 +1,13 @@
+package es.unican.is2.practica5a;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClienteTest {
+
+    @Test
+    void testPendiente() {
+        fail("Pendiente de implementar");
+    }
+}
